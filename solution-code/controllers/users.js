dae87e9ef5a91f66558edec6924ae8fcb5fc0626
Justin Castilla <pxlperfection@gmail.com>
2017-05-26 11:32:19 -0500

    refined solution

--- solution-code/controllers/users.js
+++ solution-code/controllers/users.js
@@ -1,23 +1,26 @@
-function login(request, response){
+var passport = require('passport');
+require("../config/passport")(passport)
+
+function getLogin(request, response){
   var loginStrategy = passport.authenticate('facebook');
   return loginStrategy(request, response)
 }
 
 function getCallback(request, response){
+  console.log("GOT CALL BACK");
   var callback = passport.authenticate('facebook', {
     successRedirect : '/candies',
     failureRedirect : '/candies'
   });
-
-  var callback(request, response);
+  return callback(request,response);
 }
 
-function getCallback(request, response) {
+function logout(request, response) {
   request.logout();
   response.redirect('/candies');
 }
 module.exports = {
-  login: login,
+  getLogin: getLogin,
   getCallback: getCallback,
   logout: logout
 }
